b560d771f40767a4d522aead5dd5bb711a17c9d4
ZiganshinIB <elzig3012@gmail.com>
2024-07-12 16:54:49 +0300

    2 comit

---
+++ bookmarks/account/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        label="Имя пользователя",
+        max_length=30,
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    password = forms.CharField(
+        label="Пароль",
+        required=True,
+        widget=forms.PasswordInput
+    )
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label="Пароль", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Повторите пароль", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ("username", 'first_name', 'last_name', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
---
+++ bookmarks/account/urls.py
@@ -0,0 +1,48 @@
+from django.urls import path, include
+
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    # path('login/', views.user_login, name='login'),
+
+
+    # Вход
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    # Выход
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # Сменить пароль
+    path('password_change/',
+         auth_views.PasswordChangeView.as_view(),
+         name='password_change'),
+    # Успешно изменил пароль
+    path('password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
+
+    # Сбрось пароля
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(),
+         name='password_reset'),
+    # Ссылка отправлена
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    # Ссылка подверждения для сброса пароля UID
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    # Успешно сбросил пароль
+    path('reset/complete/',
+         auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
+
+    # Расширение url для всех случии со входом в систему
+    # path('', include('django.contrib.auth.urls')),
+
+    # Регистрация
+    path('register/', views.register, name='register'),
+    # Первая страница пользователя
+    path('', views.dashboard, name='dashboard'),
+]
